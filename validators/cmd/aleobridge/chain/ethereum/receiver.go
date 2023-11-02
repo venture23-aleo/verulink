bdf29429992757cfa1bbe8495a0ad60ac4533e0d
parajuliswopnil <parajuliswopnil717@gmail.com>
2023-11-02 14:29:43 +0545

    feat: added polling in ethereum chain

--- validators/cmd/aleobridge/chain/ethereum/receiver.go
+++ validators/cmd/aleobridge/chain/ethereum/receiver.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"sort"
+	"time"
 
+	ethTypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/chain"
@@ -27,22 +30,99 @@ func NewClient(nodeUrl string) IClient {
 	return client
 }
 
-func (r *Receiver) Subscribe(ctx context.Context, startHeight int64) {
+func (r *Receiver) Subscribe(ctx context.Context, startHeight uint64) error {
 	fmt.Println("subscribe the ethereum")
 	client := NewClient("https://eth.llamarpc.com")
-	latestHeight := startHeight + 200
-	for i := startHeight; i < latestHeight; i++ {
-		fmt.Println("ethereum subscription")
-		go func(h int64) {
-			header, err := client.GetHeaderByHeight(ctx, big.NewInt(h))
-			if err!= nil {
-				return 
-			}
-			fmt.Println("ethereum", header.Number, header.Hash())
-		} (i)
+	
+	latest := func() uint64{
+		latestHeight, err := client.GetBlockNumber()
+		if err != nil {
+			return 0
+		}
+		return latestHeight
 	}
+
+	startHeight = latest()
+
+	next, latestHeight := startHeight, latest()
+
+	pollTicker := time.NewTicker(time.Minute / 2)
+
+	// msgCh := make(chan chain.Message)
+
+	bnCh := make(chan *ethTypes.Header, 10)
+	type notification struct {
+		header *ethTypes.Header
+	}
+	type query struct {
+		height uint64
+		bn     *notification
+		err error 
+	}
+	
+	for {
+		select {
+		case bn := <-bnCh:
+			fmt.Println("eth notification", bn.Number, bn.Hash())
+		case <-pollTicker.C:
+			fmt.Println("poll ticker")
+			latestHeight = latest()
+		default:
+			var headers []*notification
+			if next >= latestHeight {
+				// fmt.Println("continue")
+				continue
+			}
+
+			qchLen := min(cap(bnCh), int(latestHeight - next))
+
+			qch := make(chan *query, qchLen)
+			for i := next; i < latestHeight && len(qch) < cap(qch); i++ {
+				next++
+				qch <- &query{i, nil, nil}
+			}
+			for q := range qch {
+				switch {
+				case q.err != nil:
+					q.bn, q.err = nil, nil 
+					qch <- q
+					continue
+					
+				case q.bn != nil:
+					headers = append(headers, q.bn)
+					if len(headers) == cap(qch) {
+						close(qch)
+					}
+
+				default:
+					go func(q *query) {
+						defer func() {
+							qch <- q
+						}()
+						if q.bn == nil {
+							q.bn = &notification{}
+						}
+						header, err := client.GetHeaderByHeight(ctx, big.NewInt(int64(q.height)))
+						if err != nil {
+							q.err = err
+						}
+						q.bn.header = header
+					}(q)
+				}
+			}
+			if len(headers) > 0 {
+				sort.SliceStable(headers, func(i, j int) bool {
+					return headers[i].header.Number.Uint64() < headers[j].header.Number.Uint64()
+				})
+			}
+			for _, header := range headers {
+				bnCh <- header.header
+			}
+		}
+	}
+
 }
 
-func NewReceiver(src string, dst string, nodeAddress string) chain.IReceiver { 
-	return &Receiver{} 
+func NewReceiver(src string, dst string, nodeAddress string) chain.IReceiver {
+	return &Receiver{}
 }
