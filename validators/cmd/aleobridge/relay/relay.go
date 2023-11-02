31fc1f1d20887a5224ad86cdae6423f42067d333
parajuliswopnil <parajuliswopnil717@gmail.com>
2023-11-02 10:26:59 +0545

    feat: handle multiple destinations for a source

--- validators/cmd/aleobridge/relay/relay.go
+++ validators/cmd/aleobridge/relay/relay.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 
 	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/chain"
-	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/chain/aleo"
-	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/chain/ethereum"
 	common "github.com/venture23-aleo/aleo-bridge/validators/common/wallet"
 )
 
@@ -14,8 +12,8 @@ type SenderFunc func(src, dst, url string, wallet common.Wallet) chain.ISender
 type ReceiverFunc func(src string, dst string, nodeAddress string) chain.IReceiver
 
 var (
-	Senders   = map[string]SenderFunc{}
-	Receivers = map[string]ReceiverFunc{}
+	RegisteredSender   = map[string]SenderFunc{}
+	RegisteredReceiver = map[string]ReceiverFunc{}
 )
 
 type Relay interface {
@@ -24,14 +22,15 @@ type Relay interface {
 
 type relay struct {
 	Src chain.IReceiver
-	Dst chain.ISender
+	Dst map[string]chain.ISender
+	Cfg *ChainConfig
 }
 
 type Relays struct {
 	relay []*relay
 }
 
-func NewRelay(src chain.IReceiver, dst chain.ISender) Relay {
+func NewRelay(src chain.IReceiver, dst map[string]chain.ISender) Relay {
 	return &relay{
 		Src: src,
 		Dst: dst,
@@ -40,31 +39,57 @@ func NewRelay(src chain.IReceiver, dst chain.ISender) Relay {
 
 func MultiRelay(ctx context.Context, cfg AppConfig) *Relays {
 	multiRelay := &Relays{}
+	wallets := map[string]common.Wallet{}
+	
+	for _, r := range cfg.Chains {
+		wallet, _ := r.Wallet()
+		wallets[r.Name] = wallet
+	}
 	for _, r := range cfg.Chains {
 		switch r.Name {
 		case "aleo":
-			Receivers[r.BridgeContract] = aleo.NewReceiver
-			var dst string
-			receiver := Receivers[r.BridgeContract](r.BridgeContract, dst, r.NodeUrl)
+			sender := map[string]chain.ISender{}
+			dsts := []string{"ethereum"}
+			receiver := RegisteredReceiver["aleo"](r.BridgeContract, dsts[0], r.NodeUrl) // change
+			wallet, err := r.Wallet()
+			if err != nil {
+				return nil
+			}
+			wallets["aleo"] = wallet
 
-			Senders[r.BridgeContract] = ethereum.NewSender
-			wallet, _ := r.Wallet()
-			sender := Senders[r.BridgeContract](r.BridgeContract, dst, r.NodeUrl, wallet)
+			// handle for multiple destination
+
+			for _, dst := range dsts {
+				if dst == "ethereum" {
+					sender[dst] = RegisteredSender[dst](r.BridgeContract, dst, r.NodeUrl, wallets[dst])
+				}
+			}
 			multiRelay.relay = append(multiRelay.relay, &relay{
 				Src: receiver,
 				Dst: sender,
+				Cfg: r,
 			})
 		case "ethereum":
-			Receivers[r.BridgeContract] = ethereum.NewReceiver
-			var dst string
-			receiver := Receivers[r.BridgeContract](r.BridgeContract, dst, r.NodeUrl)
+			sender := map[string]chain.ISender{}
+			dsts := []string{"aleo"}
+			receiver := RegisteredReceiver["ethereum"](r.BridgeContract, dsts[0], r.NodeUrl)
 
-			Senders[r.BridgeContract] = aleo.NewSender
-			wallet, _ := r.Wallet()
-			sender := Senders[r.BridgeContract](r.BridgeContract, dst, r.NodeUrl, wallet)
+			wallet, err := r.Wallet()
+			if err != nil {
+				return nil
+			}
+			wallets["ethereum"] = wallet
+
+			for _, dst := range dsts {
+				if dst == "aleo" {
+					sender[dst] = RegisteredSender[dst](r.BridgeContract, dst, r.NodeUrl, wallets[dst])
+				}
+			}
+			// Senders[r.BridgeContract] =
 			multiRelay.relay = append(multiRelay.relay, &relay{
 				Src: receiver,
 				Dst: sender,
+				Cfg: r,
 			})
 		}
 	}
@@ -74,39 +99,26 @@ func MultiRelay(ctx context.Context, cfg AppConfig) *Relays {
 func (r *Relays) StartMultiRelay(ctx context.Context) {
 	relayCh := make(chan *relay, len(r.relay))
 
-	for _, relay := range(r.relay) {
+	for _, relay := range r.relay {
 		relayCh <- relay
 	}
 
-
-	// for {
-	// 	switch{
-	// 		case re = <-relayCh:
-	// 			go func(rel *relay) {
-	// 				rel.Start()
-	// 			}(re)
-				
-	// 	}
-	// }
-	
 	for {
 		select {
-		case <- ctx.Done():
+		case <-ctx.Done():
 			fmt.Println("context cancelled")
-			return 
-		case re := <- relayCh:
-			go func (relay *relay)  {
+			return
+		case re := <-relayCh:
+			go func(relay *relay) {
 				fmt.Println("started multirelay")
 				re.Start(ctx)
 			}(re)
 		}
 	}
 
-
 }
 
 func (r *relay) Start(ctx context.Context) {
 	fmt.Println("started individual relay")
-	r.Src.Subscribe(ctx)
-	r.Dst.Send(ctx)
+	r.Src.Subscribe(ctx, int64(r.Cfg.StartHeight))
 }
