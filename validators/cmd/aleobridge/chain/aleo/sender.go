45cab97c09cb9d44fa16b6444b386d9b4e249403
parajuliswopnil <parajuliswopnil717@gmail.com>
2023-11-07 13:52:45 +0545

    feat: added dummy sender for each chains

--- validators/cmd/aleobridge/chain/aleo/sender.go
+++ validators/cmd/aleobridge/chain/aleo/sender.go
@@ -2,7 +2,10 @@ package aleo
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/chain"
 	common "github.com/venture23-aleo/aleo-bridge/validators/common/wallet"
@@ -15,9 +18,17 @@ type Sender struct {
 	Client *Client
 }
 
-func (s *Sender) Send(ctx context.Context) {
-	// send according to safe and unsafe check
-	fmt.Println("send from aleo")
+func (s *Sender) Send(ctx context.Context, msg *chain.QueuedMessage) (uint64, error) {
+	fmt.Println("trying to send ", msg.DepartureBlock)
+	randomNumber := rand.Intn(10)
+	if randomNumber%3 == 0 {
+		time.Sleep(5 * time.Second)
+		return 0, errors.New("send error")
+
+	} else {
+		time.Sleep(5 * time.Second)
+		return msg.DepartureBlock, nil
+	}
 }
 
 func NewSender(src, dst, url string, wallet common.Wallet) chain.ISender { return &Sender{} }
--- validators/cmd/aleobridge/chain/ethereum/sender.go
+++ validators/cmd/aleobridge/chain/ethereum/sender.go
@@ -2,7 +2,10 @@ package ethereum
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/chain"
 	common "github.com/venture23-aleo/aleo-bridge/validators/common/wallet"
@@ -15,8 +18,18 @@ type Sender struct {
 	Client *Client
 }
 
-func (s *Sender) Send(ctx context.Context) {
-	fmt.Println("send from ethereum")
+func (s *Sender) Send(ctx context.Context, msg *chain.QueuedMessage) (uint64, error) {
+	// send according to safe and unsafe check
+	fmt.Println("trying to send ", msg.DepartureBlock)
+	randomNumber := rand.Intn(10)
+	if randomNumber%3 == 0 {
+		time.Sleep(5 * time.Second)
+		return 0, errors.New("send error")
+
+	} else {
+		time.Sleep(5 * time.Second)
+		return msg.DepartureBlock, nil
+	}
 }
 
 func NewSender(src, dst, url string, wallet common.Wallet) chain.ISender { return &Sender{} }
