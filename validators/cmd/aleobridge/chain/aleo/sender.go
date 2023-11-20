4b89840f383849b944eb1ae3aa38fa30f45840e1
parajuliswopnil <parajuliswopnil717@gmail.com>
2023-11-20 14:31:44 +0545

    feat: handelling retry logic in database

--- validators/cmd/aleobridge/chain/aleo/sender.go
+++ validators/cmd/aleobridge/chain/aleo/sender.go
@@ -4,11 +4,18 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"strconv"
 	"sync"
 	"time"
 
 	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/chain"
+	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/store"
 	common "github.com/venture23-aleo/aleo-bridge/validators/common/wallet"
+)
+
+const (
+	ALEO              = "aleo"
+	DefaultRetryCount = 1
 )
 
 type Sender struct {
@@ -18,6 +25,7 @@ type Sender struct {
 	Dst        string
 	Client     *Client
 	RetryQueue map[uint64]*chain.QueuedMessage // include err also
+	SentPackets []string 
 }
 
 func (s *Sender) Send(ctx context.Context, msg []*chain.QueuedMessage) error {
@@ -26,15 +34,32 @@ func (s *Sender) Send(ctx context.Context, msg []*chain.QueuedMessage) error {
 		if randomNumber%3 == 0 {
 			time.Sleep(5 * time.Second)
 			m.RetryCount++
-			if _, ok := s.RetryQueue[m.DepartureBlock]; !ok {
-				s.RetryQueue[m.DepartureBlock] = m
+			if m.RetryCount < DefaultRetryCount {
+				if _, ok := s.RetryQueue[m.DepartureBlock]; !ok {
+					s.RetryQueue[m.DepartureBlock] = m
+				}
+			} else {
+				depBlock := strconv.Itoa(int(m.DepartureBlock))
+				fmt.Println("putting the block in the db")
+				packet, _ := store.GetRetryPacket(ALEO, depBlock)
+				if packet != nil {
+					return nil
+				}
+				err := store.StoreRetryPacket(ALEO, depBlock, m)
+				if err != nil {
+					return err
+				}
+				delete(s.RetryQueue, m.DepartureBlock)
 			}
-			fmt.Println("couldnot send ", m.DepartureBlock)
+			fmt.Println("couldnot send ", m.DepartureBlock, m.RetryCount)
 
 		} else {
+			fmt.Println("deleting sent blocks from retry")
+			store.DeleteRetryPacket(ALEO, strconv.Itoa(int(m.DepartureBlock)))
 			delete(s.RetryQueue, m.DepartureBlock)
 			fmt.Println("message sent", m.DepartureBlock, "retry count", m.RetryCount)
 			time.Sleep(5 * time.Second)
+			s.SentPackets = append(s.SentPackets, strconv.Itoa(int(m.DepartureBlock)))
 		}
 	}
 
@@ -47,6 +72,23 @@ func (s *Sender) GetRetryingBlocks() map[uint64]*chain.QueuedMessage {
 	return s.RetryQueue
 }
 
+func (s *Sender) GetRetryingBlocksFromDB() ([]*chain.QueuedMessage, error) {
+	retryingBlocks, err := store.GetAllRetryPackets(ALEO)
+	if err != nil {
+		return nil, err
+	}
+	return retryingBlocks, nil 
+}
+
+func (s *Sender) GetRetryBlockFromDBByKey(key string) *chain.QueuedMessage{
+	msg, err := store.GetRetryPacket(ALEO, key)
+	if err != nil {
+		return nil 
+	}
+	return msg
+}
+
+
 func NewSender(src, dst, url string, wallet common.Wallet) chain.ISender {
-	return &Sender{RetryQueue: map[uint64]*chain.QueuedMessage{}}
+	return &Sender{RetryQueue: map[uint64]*chain.QueuedMessage{}, SentPackets: []string{}}
 }
--- validators/cmd/aleobridge/chain/ethereum/sender.go
+++ validators/cmd/aleobridge/chain/ethereum/sender.go
@@ -4,11 +4,18 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"strconv"
 	"sync"
 	"time"
 
 	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/chain"
+	"github.com/venture23-aleo/aleo-bridge/validators/cmd/aleobridge/store"
 	common "github.com/venture23-aleo/aleo-bridge/validators/common/wallet"
+)
+
+const (
+	DefaultRetryCount = 1
+	Ethereum          = "ethereum"
 )
 
 type Sender struct {
@@ -26,16 +33,29 @@ func (s *Sender) Send(ctx context.Context, msg []*chain.QueuedMessage) error {
 		if randomNumber%3 == 0 {
 			time.Sleep(5 * time.Second)
 			m.RetryCount++
-			if _, ok := s.RetryQueue[m.DepartureBlock]; !ok {
-				s.RetryQueue[m.DepartureBlock] =  m
+			if m.RetryCount < DefaultRetryCount {
+				if _, ok := s.RetryQueue[m.DepartureBlock]; !ok {
+					s.RetryQueue[m.DepartureBlock] = m
+				}
+			} else {
+				depBlock := strconv.Itoa(int(m.DepartureBlock))
+				fmt.Println("putting the block in the db")
+				packet, _ := store.GetRetryPacket(Ethereum, depBlock)
+				if packet != nil {
+					return nil
+				}
+				err := store.StoreRetryPacket(Ethereum, depBlock, m)
+				if err != nil {
+					return err
+				}
+				delete(s.RetryQueue, m.DepartureBlock)
 			}
 			fmt.Println("couldnot send ", m.DepartureBlock, m.RetryCount)
 
 		} else {
-			if _, ok := s.RetryQueue[m.DepartureBlock]; ok {
-				fmt.Println("deleting sent blocks from retry")
-				delete(s.RetryQueue, m.DepartureBlock)
-			}
+			fmt.Println("deleting sent blocks from retry")
+			store.DeleteRetryPacket(Ethereum, strconv.Itoa(int(m.DepartureBlock)))
+			delete(s.RetryQueue, m.DepartureBlock)
 			fmt.Println("message sent", m.DepartureBlock, "retry count", m.RetryCount)
 			time.Sleep(5 * time.Second)
 		}
@@ -48,6 +68,22 @@ func (s *Sender) GetRetryingBlocks() map[uint64]*chain.QueuedMessage {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
 	return s.RetryQueue
+}
+
+func (s *Sender) GetRetryBlockFromDBByKey(key string) *chain.QueuedMessage {
+	msg, err := store.GetRetryPacket(Ethereum, key)
+	if err != nil {
+		return nil
+	}
+	return msg
+}
+
+func (s *Sender) GetRetryingBlocksFromDB() ([]*chain.QueuedMessage, error) {
+	retryingBlocks, err := store.GetAllRetryPackets(Ethereum)
+	if err != nil {
+		return nil, err
+	}
+	return retryingBlocks, nil
 }
 
 func NewSender(src, dst, url string, wallet common.Wallet) chain.ISender {
