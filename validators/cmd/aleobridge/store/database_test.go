3bef355c5291c55e35c18a2fed4ab77f4b647027
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2023-11-23 19:53:41 +0545

    Remove tests

--- validators/cmd/aleobridge/store/database_test.go
+++ validators/cmd/aleobridge/store/database_test.go
@@ -1,7 +1,6 @@
 package store
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,25 +19,6 @@ func TestDatabase(t *testing.T) {
 	dbVal, err := GetRetryPacket("aleo", key)
 	assert.Nil(t, err)
 	assert.Equal(t, 50, dbVal.RetryCount)
-}
-
-func TestGetDatabaseValue(t *testing.T) {
-	InitKVStore("retrydb")
-	key := "507884"
-	value, err := GetRetryPacket("ethereum", key)
-	fmt.Println(value.DepartureBlock)
-	assert.Nil(t, err)
-}
-
-func TestGetALLDBValue(t *testing.T) {
-	InitKVStore("retrydb")
-	value, err := GetAllRetryPackets("ethereum")
-	fmt.Println("value len is:", len(value))
-
-	for _, v := range(value) {
-		fmt.Println("depart block:", v.DepartureBlock)
-	}
-	assert.Nil(t, err)
 }
 
 func TestDatabaseDelete(t *testing.T) {
