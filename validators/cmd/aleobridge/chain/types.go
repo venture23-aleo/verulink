b2a7bec27f201bf2335c2cc1e7f45ce6d5b6b76a
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2023-12-13 19:27:54 +0545

    Add func in interface and modify data type

--- validators/cmd/aleobridge/chain/types.go
+++ validators/cmd/aleobridge/chain/types.go
@@ -21,6 +21,13 @@ type ISender interface {
 	// seems not required
 	// GetLatestHeight(ctx context.Context) (uint64, error)
 	IsTxnFinalized(ctx context.Context, txnHash string) (bool, error)
+
+	// GetMinReqBalForMakingTxn returns minimum balance required to make a transaction.
+	// Since size of transaction is fixed, it should return same value which also means
+	// sender can store it in some struct field
+	GetMinReqBalForMakingTxn() int
+	// GetWalletBalance returns current balance of a wallet and error if encounters any
+	GetWalletBalance(ctx context.Context) (int, error)
 }
 
 type IReceiver interface {
@@ -58,7 +65,7 @@ type Packet struct {
 type TxnPacket struct {
 	// Ts is timestamp as byte which is used as key to store in key-value db.
 	// It is assigned by Storing function and will be populated in struct when retrieving from the db
-	TSByte  []byte `json:"-"`
+	SeqByte []byte `json:"-"`
 	TxnHash string
 	Pkt     *Packet
 }
