dfde53fa4fc9b0a3f9398bc05b5b2b88196a654e
biplavIbriz <biplav.osti@ibriz.ai>
2023-12-28 19:31:04 +0545

    ERC20TokenService unit tests for Blacklist and TokenSupport

--- solidity/test/ERC20TokenService.test.js
+++ solidity/test/ERC20TokenService.test.js
@@ -244,4 +244,257 @@ describe('ERC20TokenService', () => {
         expect (await usdcMock.balanceOf(proxiedV1)).to.be.equal(0);
         expect (await usdcMock.balanceOf(other.address)).to.be.equal(150);
     }); 
+
+    //----------------- BlackListService ----------------
+
+    it("should add to and remove from the black list", async () => {
+        // Check initial status
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.false;
+
+        // Add to the black list
+        await proxiedV1.addToBlackList(other.address);
+
+        // Check if added
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.true;
+
+        // Remove from the black list
+        await proxiedV1.removeFromBlackList(other.address);
+
+        // Check if removed
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.false;
+    });
+
+    it("should add to and remove from the black list for usdc", async () => {
+        // Check initial status
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.false;
+
+        // Add to the black list
+        await usdcMock.addBlackList(other.address);
+
+        // Check if added
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.true;
+
+        // Remove from the black list
+        await usdcMock.removeBlackList(other.address);
+
+        // Check if removed
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.false;
+    });
+
+    it("should add to and remove from the black list for usdt", async () => {
+        // Check initial status
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.false;
+
+        // Add to the black list
+        await usdTMock.addBlackList(other.address);
+
+        // Check if added
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.true;
+
+        // Remove from the black list
+        await usdTMock.removeBlackList(other.address);
+
+        // Check if removed
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.false;
+    });
+
+    it("should allow only owner to add to the black list", async () => {
+        // Try to add to the black list with the owner
+        await proxiedV1.connect(owner).addToBlackList(other.address);
+
+        // Check if the account is added to the black list
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.true;
+
+        // Try to add to the black list with another account and expect it to revert
+        await expect(
+            proxiedV1.connect(other).addToBlackList(owner.address)
+        ).to.be.reverted;
+    });
+
+    it("should allow only owner to remove from the black list", async () => {
+        // Add to the black list with the owner
+        await proxiedV1.connect(owner).addToBlackList(other.address);
+
+        // Try to remove from the black list with the owner
+        await proxiedV1.connect(owner).removeFromBlackList(other.address);
+
+        // Check if the account is removed from the black list
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.false;
+
+        // Try to remove from the black list with another account and expect it to revert
+        await expect(
+            proxiedV1.connect(other).removeFromBlackList(owner.address)
+        ).to.be.reverted;
+    });
+
+    it("should include USDC and USDT blacklists", async () => {
+        // Mock USDC and USDT contracts to simulate blacklisting
+        await usdcMock.addBlackList(other.address);
+
+        // Check if blacklisted in BlackListService
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.true;
+
+        // Remove from blacklists
+        await usdcMock.removeBlackList(other.address);
+
+        // Check if removed from BlackListService
+        expect(await proxiedV1.isBlackListed(other.address)).to.be.false;
+    });
+
+
+    //-------------- ERC20TokenSupport-----------
+
+    it('should add a token', async () => {
+        const tokenAddress = ethers.Wallet.createRandom().address;
+        const destChainId = 1;
+        const destTokenAddress = "0x123";
+        const destTokenService = "aleo.bridge";
+        const min = 1;
+        const max = 100;
+
+        // Add token
+        await proxiedV1.addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max);
+
+        // Check if the token was added
+        const isSupported = await proxiedV1.isSupportedToken(tokenAddress);
+        expect(isSupported).to.be.true;
+
+        // Check if the added token matches the expected values
+        const addedToken = await proxiedV1.supportedTokens(tokenAddress);
+        expect(addedToken.tokenAddress).to.equal(tokenAddress);
+        expect(addedToken.destTokenAddress.chainId).to.equal(destChainId);
+        expect(addedToken.destTokenAddress.addr).to.equal(destTokenAddress);
+        expect(addedToken.destTokenService.chainId).to.equal(destChainId);
+        expect(addedToken.destTokenService.addr).to.equal(destTokenService);
+        expect(addedToken.minValue).to.equal(min);
+        expect(addedToken.maxValue).to.equal(max);
+        expect(addedToken.enabled).to.be.true;
+    });
+
+    // Test attempting to add an existing token
+    // it('should revert when trying to add an existing token', async () => {
+    //     const tokenAddress = ethers.Wallet.createRandom().address;
+    //     const destChainId = 1;
+    //     const destTokenAddress = "0x123";
+    //     const destTokenService = "aleo.bridge";
+    //     const min = 1;
+    //     const max = 100;
+
+    //     // Add a token
+    //     await proxiedV1.addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max);
+    //     console.log("hello");
+
+    //     // Attempt to add the same token again
+    //     await expect(proxiedV1.addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max))
+    //         .to.be.revertedWith('Token already supported');
+    // });
+
+    // Test removing a token
+    it('should remove a token', async () => {
+        const tokenAddress = ethers.Wallet.createRandom().address;
+        const destChainId = 1;
+        const destTokenAddress = "0x123";
+        const destTokenService = "aleo.bridge";
+        const min = 1;
+        const max = 100;
+
+        // Add token
+        await proxiedV1.addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max);
+
+        // Remove token
+        await proxiedV1.removeToken(tokenAddress);
+
+        // Check if the token was removed
+        const isSupported = await proxiedV1.isSupportedToken(tokenAddress);
+        expect(isSupported).to.be.false;
+    });
+
+    // Test attempting to remove a non-existing token
+    it('should revert when trying to remove a non-existing token', async () => {
+        const nonExistingToken = ethers.Wallet.createRandom().address;
+
+        // Attempt to remove a non-existing token
+        await expect(proxiedV1.removeToken(nonExistingToken)).to.be.revertedWith('Token not supported');
+    });
+
+    // ...
+
+// Test isSupportedToken function
+it('should return true for a supported token and matching destChainId', async () => {
+    const tokenAddress = ethers.Wallet.createRandom().address;
+    const destChainId = 1;
+    const destTokenAddress = "0x123";
+    const destTokenService = "aleo.bridge";
+    const min = 1;
+    const max = 100;
+
+    // Add token
+    await proxiedV1.addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max);
+
+    // Check if the token is supported for the specified destChainId
+    const isSupported = await proxiedV1["isSupportedToken(address, uint256)"](tokenAddress, destChainId);
+    expect(isSupported).to.be.true;
+});
+
+it('should return false for a supported token and non-matching destChainId', async () => {
+    const tokenAddress = ethers.Wallet.createRandom().address;
+    const destChainId = 1;
+    const destTokenAddress = "0x123";
+    const destTokenService = "aleo.bridge";
+    const min = 1;
+    const max = 100;
+
+    // Add token
+    await proxiedV1.addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max);
+
+    // Check if the token is not supported for a different destChainId
+    const nonMatchingDestChainId = 2;
+    const isSupported = await proxiedV1["isSupportedToken(address, uint256)"](tokenAddress, nonMatchingDestChainId);
+    expect(isSupported).to.be.false;
+});
+
+it('should return false for a non-existing token and any destChainId', async () => {
+    const nonExistingToken = ethers.Wallet.createRandom().address;
+    const destChainId = 1;
+
+    // Check if the non-existing token is not supported for any destChainId
+    const isSupported = await proxiedV1["isSupportedToken(address, uint256)"](nonExistingToken, destChainId);
+    expect(isSupported).to.be.false;
+});
+
+    // Test onlyOwner modifier for addToken function
+    it('should allow only owner to add a token', async () => {
+        const tokenAddress = ethers.Wallet.createRandom().address;
+        const destChainId = 1;
+        const destTokenAddress = '0x123';
+        const destTokenService = 'aleo.bridge';
+        const min = 1;
+        const max = 100;
+
+        // Add token with the owner
+        await proxiedV1.addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max);
+
+        // Try to add token with another account and expect it to revert
+        await expect(
+            proxiedV1.connect(other).addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max)
+        ).to.be.reverted;
+    });
+
+    // Test onlyOwner modifier for removeToken function
+    it('should allow only owner to remove a token', async () => {
+        const tokenAddress = ethers.Wallet.createRandom().address;
+        const destChainId = 1;
+        const destTokenAddress = '0x123';
+        const destTokenService = 'aleo.bridge';
+        const min = 1;
+        const max = 100;
+
+        // Add token with the owner
+        await proxiedV1.addToken(tokenAddress, destChainId, destTokenAddress, destTokenService, min, max);
+
+        // Try to remove token with another account and expect it to revert
+        await expect(
+            proxiedV1.connect(other).removeToken(tokenAddress)
+        ).to.be.reverted;
+    });
 });
