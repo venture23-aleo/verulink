d4739a66f58b8a356efea03a1c745c79c7337dc0
biplavIbriz <biplav.osti@ibriz.ai>
2023-12-30 15:38:51 +0545

    added contract-sizer plugin

--- solidity/hardhat.config.cjs
+++ solidity/hardhat.config.cjs
@@ -1,13 +1,32 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-contract-sizer");
 require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+            details: { yul: false },
+          }
+        }
+      }
+    ]
+  },
   networks: {
     goerli: {
       url: "https://eth-goerli.g.alchemy.com/v2/fLCeKO4GA9Gc3js8MUt9Djy7WHCFxATq",
       accounts: [process.env.SECRET_KEY1]
     }
+  },
+  
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true
   }
 };
