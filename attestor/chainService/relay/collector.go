12ffcc861b8c45983bb3216d3bc6bfa902d22ebd
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2024-01-20 18:59:10 +0545

    Add skeleton for sending signature

--- attestor/chainService/relay/collector.go
+++ attestor/chainService/relay/collector.go
@@ -2,7 +2,26 @@ package relay
 
 import "github.com/venture23-aleo/attestor/chainService/chain"
 
+// params
+const (
+	srcChainID  = "src_chain_id"
+	destChainID = "dest_chain_id"
+	seqNum      = "seq_num"
+	signature   = "signature"
+	isWhite     = "is_white"
+)
+
 func sendToCollector(sp *chain.ScreenedPacket, signature string) error {
+
+	params := map[string]interface{}{
+		srcChainID:  sp.Packet.Source.ChainID,
+		destChainID: sp.Packet.Destination.ChainID,
+		seqNum:      sp.Packet.Sequence,
+		signature:   signature,
+		isWhite:     sp.IsWhite,
+	}
+
+	_ = params
 	// todo: need to send srcChainId, destChainID, packetSeqNum, signature and probably isWhite
 	return nil
 }
