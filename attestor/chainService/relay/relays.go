6041242fe81560076c5303f9ffa944e6ec705d9b
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2024-01-22 09:57:39 +0545

    Remove redundant channel

--- attestor/chainService/relay/relays.go
+++ attestor/chainService/relay/relays.go
@@ -33,42 +33,35 @@ func StartRelay(ctx context.Context, cfg *config.Config) {
 	}
 
 	pktCh := make(chan *chain.Packet)
-	doneCh := make(chan struct{})
-	go initPacketFeeder(ctx, cfg.ChainConfigs, pktCh, doneCh)
-	<-doneCh
+	go initPacketFeeder(ctx, cfg.ChainConfigs, pktCh)
 	consumePackets(ctx, pktCh)
 }
 
-func initPacketFeeder(ctx context.Context, cfgs []*config.ChainConfig, pktCh chan<- *chain.Packet, doneCh chan struct{}) {
+func initPacketFeeder(ctx context.Context, cfgs []*config.ChainConfig, pktCh chan<- *chain.Packet) {
 	ch := make(chan chain.IClient, len(cfgs))
-	go func() {
-		for _, chainCfg := range cfgs {
-			if _, ok := RegisteredClients[chainCfg.Name]; !ok {
-				panic(fmt.Sprintf("module undefined for chain %s", chainCfg.Name))
-			}
-
-			if _, ok := RegisteredHashers[chainCfg.Name]; !ok {
-				panic(fmt.Sprintf("hash undefined for chain %s", chainCfg.Name))
-			}
-			ch <- RegisteredClients[chainCfg.Name](chainCfg)
-			chainIDToChainName[chainCfg.ChainID] = chainCfg.Name
+	for _, chainCfg := range cfgs {
+		if _, ok := RegisteredClients[chainCfg.Name]; !ok {
+			panic(fmt.Sprintf("module undefined for chain %s", chainCfg.Name))
 		}
-		close(doneCh)
-	}()
 
-	<-doneCh
+		if _, ok := RegisteredHashers[chainCfg.Name]; !ok {
+			panic(fmt.Sprintf("hash undefined for chain %s", chainCfg.Name))
+		}
+		ch <- RegisteredClients[chainCfg.Name](chainCfg)
+		chainIDToChainName[chainCfg.ChainID] = chainCfg.Name
+	}
 
 	for chain := range ch {
 		chain := chain
 		go func() {
 			ctx, cncl := context.WithCancel(ctx)
+			defer cncl()
 			defer func() {
 				if r := recover(); r != nil {
 					cncl()
 					ch <- chain
 				}
 			}()
-			defer cncl()
 			chain.FeedPacket(ctx, pktCh)
 		}()
 	}
