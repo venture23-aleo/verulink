4acd14d759b86619ba4fdff82bcf9068ba91ddd8
Laxmi Prasad Oli <olilaxmiprasad@gmail.com>
2024-01-22 21:31:35 +0545

    Move errors.go to common package

---
+++ attestor/chainService/common/wallet/errors.go
@@ -0,0 +1,28 @@
+package common
+
+import (
+	"fmt"
+)
+
+type TimeOutErr struct {
+}
+
+func (TimeOutErr) Error() string {
+	return "timeout"
+}
+
+type InvalidSignatureErr struct {
+	PktHash   string
+	Signature string
+}
+
+func (e InvalidSignatureErr) Error() string {
+	return fmt.Sprintf("signature %s is invalid for hash %s", e.Signature, e.PktHash)
+}
+
+type AlreadyRelayedPacket struct {
+}
+
+func (e AlreadyRelayedPacket) Error() string {
+	return fmt.Sprintf("packet aleady sent")
+}
