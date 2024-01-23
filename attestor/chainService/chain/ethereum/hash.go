7a96a8bef06b7b3d2de2b3fe8db8a0d97d20208f
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-01-23 09:42:24 +0545

    feat: added packet hashing feature for messages to ethereum

--- attestor/chainService/chain/ethereum/hash.go
+++ attestor/chainService/chain/ethereum/hash.go
@@ -1,7 +1,53 @@
 package ethereum
 
-import "github.com/venture23-aleo/attestor/chainService/chain"
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/venture23-aleo/attestor/chainService/chain"
+)
 
 func hash(sp *chain.ScreenedPacket) string {
-	return ""
+	packet := sp.Packet
+
+	versionBytes := make([]byte, 32)
+	packet.Version.FillBytes(versionBytes)
+	sequenceBytes := make([]byte, 32)
+	packet.Sequence.FillBytes(sequenceBytes)
+	heightBytes := make([]byte, 32)
+	packet.Height.FillBytes(heightBytes)
+	amountBytes := make([]byte, 32)
+	packet.Message.Amount.FillBytes(amountBytes)
+	srcChainIDBytes := make([]byte, 32)
+	packet.Source.ChainID.FillBytes(srcChainIDBytes)
+	dstChainIDBytes := make([]byte, 32)
+	packet.Destination.ChainID.FillBytes(dstChainIDBytes)
+
+	hash := crypto.Keccak256Hash(
+		versionBytes,
+		sequenceBytes,
+		srcChainIDBytes,
+		[]byte(packet.Source.Address),
+		dstChainIDBytes,
+		common.Hex2Bytes(packet.Destination.Address),
+		[]byte(packet.Message.SenderAddress),
+		common.Hex2Bytes(packet.Message.DestTokenAddress),
+		amountBytes,
+		common.Hex2Bytes(packet.Message.ReceiverAddress),
+		heightBytes,
+	)
+
+	voteByte := make([]byte, 1)
+	if sp.IsWhite {
+		yay := big.NewInt(1)
+		yay.FillBytes(voteByte)
+	} else {
+		nay := big.NewInt(0)
+		nay.FillBytes(voteByte)
+	}
+
+	finalHash := crypto.Keccak256Hash([]byte(fmt.Sprintf("\x19Ethereum Signed Message:\n%d", len(hash))), hash.Bytes(), voteByte)
+	return finalHash.Hex()
 }
