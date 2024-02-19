fb8620c3d3a82b104f43ba3dd47d8cd1251d667d
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-02-19 17:30:37 +0545

    fix: fixed dir path

--- attestor/signingService/build_docker.sh
+++ attestor/signingService/build_docker.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
+cd ..
 export DIR=$(pwd)
-
 docker build -f $DIR/signingService/Dockerfile -t signingservice $DIR
 
-docker compose -f $DIR/signingService/compose.yaml up
+docker compose -f $DIR/signingService/compose.yaml up -d
