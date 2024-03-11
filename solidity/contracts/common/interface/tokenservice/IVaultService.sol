dd9fde4766421b3fddaf72e60ebf5c059ba96dc0
Bishal Bist <bishal.bist@ibriz.ai>
2024-03-11 13:17:10 +0545

    reverted IVaultService.sol

--- solidity/contracts/common/interface/tokenservice/IVaultService.sol
+++ solidity/contracts/common/interface/tokenservice/IVaultService.sol
@@ -4,6 +4,15 @@ pragma solidity ^0.8.19;
 /// @title IVaultService
 /// @dev Interface for interacting with a Vault Service.
 interface IVaultService {
+    
+    /// @notice Returns the address of the associated token.
+    /// @return address of the token.
+    function token() external view returns (address);
+
+    /// @notice Returns the name of the Vault Service.
+    /// @return name of the Vault Service.
+    function name() external view returns (string memory);
+
     /// @notice Transfers a specified amount of tokens.
     /// @param amount The amount of tokens to transfer.
     /// @return boolean indicating whether the transfer was successful.
