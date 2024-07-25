525fd55382646d795d4ede374cedc5337d622a4f
nanney <nanneychan03@gmail.com>
2024-07-25 17:54:23 +0545

    chore: staging mode added

--- attestor/chainService/config/config.go
+++ attestor/chainService/config/config.go
@@ -150,6 +150,8 @@ func InitConfig(flagArgs *FlagArgs) error {
 
 	if flagArgs.Mode == Production {
 		config.Mode = flagArgs.Mode
+	} else if flagArgs.Mode == "staging"{ // TODO: this change is only for staging should be removed on production
+		config.Mode = flagArgs.Mode
 	} else {
 		config.Mode = Development
 	}
