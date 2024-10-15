2f6839e4a37601543207d053fcc6298a63fce703
Ayush Aggarwal <aagfye@gmail.com>
2024-10-14 19:57:13 -0700

    chore: forge init

--- solidity/hardhat.config.cjs
+++ solidity/hardhat.config.cjs
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-foundry");
 require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-contract-sizer");
