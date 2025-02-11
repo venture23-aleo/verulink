15ff06583d92343cc08de4751f2cef2690769d76
sdpbhandari <bhandari.sudeep68@gmail.com>
2025-02-11 22:45:07 +0545

    update transfer script with BigNumber

--- solidity/scripts/deploy/transferFundsToTokenService.js
+++ solidity/scripts/deploy/transferFundsToTokenService.js
@@ -1,6 +1,8 @@
 import hardhat from 'hardhat';
-const { ethers } = hardhat;
+
+const {ethers} = hardhat;
 import * as dotenv from "dotenv";
+import {BigNumber} from 'ethers';
 
 dotenv.config();
 
@@ -18,21 +20,23 @@ async function main() {
     const usdtTokenAddress = await ethers.getContractFactory("USDTMock");
     const usdtTokenContract = new ethers.Contract(usdtToken, usdtTokenAddress.interface.format(), deployerSigner);
 
-    const ERC20TokenService = await ethers.getContractFactory("TokenServiceV2");
+    const ERC20TokenService = await ethers.getContractFactory("TokenService");
     const tokenServiceProxyAddress = process.env.TOKENSERVICE_PROXY_ADDRESS;
     const TokenServiceABI = ERC20TokenService.interface.format();
     const TokenServiceContract = new ethers.Contract(tokenServiceProxyAddress, TokenServiceABI, deployerSigner);
 
     console.log("Transfering USDC to tokenservice...");
-    await usdcTokenContract["approve(address,uint256)"](tokenServiceProxyAddress, 100000000);
-    await usdtTokenContract["approve(address,uint256)"](tokenServiceProxyAddress, 100000000);
+    const receiver = "aleo1hkjqvh3qn4q3lr2sx5wqkt57c7heq826583duc6nlhfctkheyu8sf2qknh";
+    await usdcTokenContract["approve(address,uint256)"](tokenServiceProxyAddress, BigNumber.from("900000000000")); // approving 900K USDC to tokenservice
+    await usdtTokenContract["approve(address,uint256)"](tokenServiceProxyAddress, BigNumber.from("900000000000")); // approving 900K USDT to tokenservice
 
-    await TokenServiceContract["transfer(address,uint256,string)"](usdcToken, 100000000, "aleo1hkjqvh3qn4q3lr2sx5wqkt57c7heq826583duc6nlhfctkheyu8sf2qknh", {gasLimit: 1000000});
-    await TokenServiceContract["transfer(address,uint256,string)"](usdtToken, 100000000, "aleo1hkjqvh3qn4q3lr2sx5wqkt57c7heq826583duc6nlhfctkheyu8sf2qknh", {gasLimit: 1000000});
-    await TokenServiceContract["transfer(string)"]("aleo1fg8y0ax9g0yhahrknngzwxkpcf7ejy3mm6cent4mmtwew5ueps8s6jzl27", { value: ethers.utils.parseEther("0.01"),  gasLimit:1000000 });
+    await TokenServiceContract["transfer(address,uint256,string)"](usdcToken, BigNumber.from("900000000000"), receiver, {gasLimit: 1000000});
+    await TokenServiceContract["transfer(address,uint256,string)"](usdtToken, BigNumber.from("900000000000"), receiver, {gasLimit: 1000000});
+    await TokenServiceContract["transfer(string)"](receiver, { value: ethers.utils.parseEther("0.01"),  gasLimit:1000000 });
 
     console.log("USDC transferred successfully!!!");
 }
+
 main()
     .then(() => process.exit(0))
     .catch((error) => {
