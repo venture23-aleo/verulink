5357d50684905c658b63df724182cbb16ff4c267
sdpbhandari <bhandari.sudeep68@gmail.com>
2025-02-12 00:36:25 +0545

    update hardhat config for cicd setup

--- solidity/hardhat.config.cjs
+++ solidity/hardhat.config.cjs
@@ -2,62 +2,63 @@ require("@nomicfoundation/hardhat-toolbox");
 require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-etherscan");
 require("hardhat-contract-sizer");
+require('solidity-coverage')
 require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.19",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 400,
-            details: { yul: false },
-          }
-        }
-      }
-    ]
-  },
-  networks: {
-    sepolia: {
-      url: process.env.PROVIDER,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY]
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.19",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 400,
+                        details: {yul: false},
+                    }
+                }
+            }
+        ]
     },
-    mainnet: {
-      url: process.env.PROVIDER,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY]
-    },
-    holesky: {
-      url: process.env.PROVIDER,
-      accounts: [
-        process.env.DEPLOYER_PRIVATE_KEY]
-    }
-  },
-  etherscan: {
-    apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY,
-      holesky: process.env.ETHERSCAN_API_KEY,
-    },
-    customChains: [
-      {
-        network: "holesky",
-        chainId: 17000,
-        urls: {
-          apiURL: "https://api-holesky.etherscan.io/api",
-          browserURL: "https://holesky.etherscan.io/",
+    networks: {
+        hardhat: {
+            allowUnlimitedContractSize: true,
         },
-      },
-    ]
-  },
-  // contractSizer: {
-  //   alphaSort: true,
-  //   runOnCompile: true
-  // },
-  mocha: {
-    timeout: 100000000
-  },
+        sepolia: {
+            url: process.env.PROVIDER || "",
+            accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+        },
+        holesky: {
+            url: process.env.PROVIDER || "",
+            accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+        },
+        mainnet: {
+            url: process.env.PROVIDER || "",
+            accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+        }
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: process.env.ETHERSCAN_API_KEY,
+            holesky: process.env.ETHERSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "holesky",
+                chainId: 17000,
+                urls: {
+                    apiURL: "https://api-holesky.etherscan.io/api",
+                    browserURL: "https://holesky.etherscan.io/",
+                },
+            },
+        ]
+    },
+    // contractSizer: {
+    //   alphaSort: true,
+    //   runOnCompile: true
+    // },
+    mocha: {
+        timeout: 100000000
+    },
 };
