c640dee9a8827180e0ea72990868969927fb51e8
sdpbhandari <bhandari.sudeep68@gmail.com>
2025-04-02 22:52:00 +0545

    add script to deploy version 3 tokenService

---
+++ solidity/scripts/deploy/deployTokenServiceV3Impl.js
@@ -0,0 +1,33 @@
+import hardhat from 'hardhat';
+const { ethers, run } = hardhat;
+import * as dotenv from "dotenv";
+dotenv.config();
+import { updateEnvFile } from "../multisig/utils.js";
+
+async function main() {
+    const provider = new ethers.providers.JsonRpcProvider(
+        process.env.PROVIDER
+    );
+   
+    const TokenService = await ethers.getContractFactory("TokenServiceV3");
+
+    console.log("Deploying TokenServiceV3");
+
+    const tokenServiceImpl = await TokenService.deploy();
+    await tokenServiceImpl.deployTransaction.wait(3);
+    console.log("TokenService Impl Deployed to: ", tokenServiceImpl.address);
+    // Verification process
+    console.log("Verifying impl contract...");
+    await run("verify:verify", {
+        address: tokenServiceImpl.address,
+        constructorArguments: [], // Pass the constructor arguments here
+        contract: "contracts/main/tokenservice/TokenServiceV3.sol:TokenServiceV3"
+    });
+    updateEnvFile("TOKENSERVICE_NEW_IMPLEMENTATION_ADDRESS", tokenServiceImpl.address);
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
