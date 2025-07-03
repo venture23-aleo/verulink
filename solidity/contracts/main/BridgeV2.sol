3cb083e5b957d49dc23ee1da26a3491bc99fde30
sdpbhandari <bhandari.sudeep68@gmail.com>
2025-07-03 12:15:56 +0545

    Update BridgeV2 contract to use AttestorManagerV2 and modify constructor for upgradeability. Remove initializer function and implement a custom constructor to disable initializers.
    fix #25

--- solidity/contracts/main/BridgeV2.sol
+++ solidity/contracts/main/BridgeV2.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import {AleoAddressLibrary} from "../common/libraries/AleoAddressLibrary.sol";
 import {PacketLibrary} from "../common/libraries/PacketLibrary.sol";
 import {Pausable} from "../common/Pausable.sol";
-import {AttestorManager} from "../base/bridge/AttestorManager.sol";
+import {AttestorManagerV2} from "../base/bridge/AttestorManagerV2.sol";
 import {BridgeTokenServiceManager} from "../base/bridge/BridgeTokenServiceManager.sol";
 import {ConsumedPacketManagerImpl} from "../base/bridge/ConsumedPacketManagerImpl.sol";
 import {OutgoingPacketManagerImplV2} from "../base/bridge/OutgoingPacketmanagerImplV2.sol";
@@ -17,7 +17,7 @@ import {Upgradeable} from "@thirdweb-dev/contracts/extension/Upgradeable.sol";
 contract BridgeV2 is 
     OwnableUpgradeable,
     Pausable,
-    AttestorManager,
+    AttestorManagerV2,
     BridgeTokenServiceManager,
     ConsumedPacketManagerImpl,
     OutgoingPacketManagerImplV2,
@@ -33,16 +33,9 @@ contract BridgeV2 is
     /// @notice The destination chain ID for packet routing
     uint256 public destinationChainId;
 
-    /// @dev Initializes the Bridge contract
-    /// @param _destChainId The initial destination chain ID
-    function Bridge_init(
-        uint256 _destChainId,
-        address _owner
-    ) public initializer {
-        __Ownable_init_unchained();
-        __Pausable_init_unchained();
-        destinationChainId = _destChainId;
-        _transferOwnership(_owner);      
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
     function _authorizeUpgrade(address) internal virtual view override {
