438d73472f091e6e6d150b304a1bbe156eeac987
naneey <nanneychan03@gmail.com>
2025-07-21 10:26:14 +0545

    fix: removed unsafe typecast

--- attestor/chainService/store/constraint.go
+++ attestor/chainService/store/constraint.go
@@ -5,15 +5,12 @@ import (
 )
 
 type keyConstraint interface {
-	int64 | uint64 | ~string | ~[]byte
+	uint64 | ~string | ~[]byte
 }
 
 func getKeyByteForKeyConstraint[T keyConstraint](key T) (k []byte) {
 	var i interface{} = key
 	switch v := i.(type) {
-	case int64:
-		k = make([]byte, 8)
-		binary.BigEndian.PutUint64(k, uint64(v))
 	case uint64:
 		k = make([]byte, 8)
 		binary.BigEndian.PutUint64(k, v)
@@ -29,8 +26,6 @@ func convertType[T keyConstraint](returnType T, b []byte) T {
 	var i any = returnType
 	var v any
 	switch i.(type) {
-	case int64:
-		v = int64(binary.BigEndian.Uint64(b))
 	case uint64:
 		v = binary.BigEndian.Uint64(b)
 	case string:
